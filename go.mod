8bef8e0a766d0667fcd6f46fb80184cc0f86da9f
zhengkeyan@bjca.org.cn <zhengkeyan@bjca.org.cn>
2021-02-08 14:11:57 +0800

    frist

---
+++ go.mod
@@ -0,0 +1,9 @@
+module goDownload
+
+go 1.15
+
+require (
+	github.com/cavaliercoder/grab v2.0.0+incompatible
+	github.com/gin-gonic/gin v1.6.3
+	github.com/tidwall/gjson v1.6.8
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/cavaliercoder/grab"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	Dir  string
+	Resp *grab.Response
+)
+
+func main() {
+	Dir = *flag.String("d", "/mnt/share/Download", "Save File Directory") //nolint
+	Port := flag.String("p", "1018", "Run Port")
+	flag.Parse()
+
+	r := gin.Default()
+	r.POST("download", Download)
+	r.GET("state", State)
+
+	r.Run(":" + *Port)
+	log.Fatal("端口被占用")
+}
+
+func State(c *gin.Context) {
+	if Resp == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"size":     "",
+			"status":   "OK",
+			"message":  "无下载任务",
+			"filename": "",
+		})
+		return
+	}
+	if Resp.IsComplete() {
+		if Resp.Err() != nil {
+			c.JSON(Resp.HTTPResponse.StatusCode, gin.H{
+				"status":  Resp.HTTPResponse.Status,
+				"message": "下载失败",
+				"error":   Resp.Err().Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"size":     Unwrap(Resp.BytesComplete()),
+			"status":   "OK",
+			"message":  "下载完成",
+			"filename": filepath.Base(Resp.Filename),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"size":     Unwrap(Resp.BytesComplete()),
+		"status":   "OK",
+		"message":  "下载中...",
+		"filename": filepath.Base(Resp.Filename),
+	})
+}
+
+func Download(c *gin.Context) {
+	jsonStr := make(map[string]string) //注意该结构接受的内容
+	if c.BindJSON(&jsonStr) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusText(http.StatusBadRequest),
+			"message": "参数不正确",
+		})
+		return
+	}
+
+	if Resp != nil {
+		if !Resp.IsComplete() {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  http.StatusText(http.StatusBadRequest),
+				"message": "仍有下载任务未完成, 不支持多任务下载",
+			})
+			return
+		}
+	}
+
+	client := grab.NewClient()
+	req, err := grab.NewRequest(Dir, jsonStr["link"])
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusText(http.StatusBadRequest),
+			"message": "不支持的地址",
+			"error":   err.Error(),
+		})
+		return
+	}
+	go func() {
+		Resp = client.Do(req)
+	}()
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "OK",
+		"message": "开始下载",
+	})
+}
+
+func Unwrap(num int64) string {
+	f := float64(num) / 1024 / 1024
+	return fmt.Sprintf("%.2fM", f)
+}
